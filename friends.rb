8320b083100f97a082706ebf73902d96cb6c6a2e
Sooyung Kim <kimx1279@gmail.com>
2023-08-02 13:27:06 -0600

    Add places to visit

--- friends.rb
+++ friends.rb
@@ -2,3 +2,8 @@ arrays = ["Cydnee", "Xander", "Sam", "Brendan", "Billy"]
 puts arrays[2]
 puts arrays.last
 puts arrays.first
+
+places = ["Japan", "Hawaii", "Italy", "France", "Portugal"]
+places.each do |list|
+    puts list.reverse
+end
